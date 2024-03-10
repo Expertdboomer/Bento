b3cd859a9c98bdfb23f89ede9bbb628ada5d5d6c
Expertdboomer <162906403+Expertdboomer@users.noreply.github.com>
2024-03-11 01:10:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '5f4dd48ba8481b2a480ecd991a82cfb2', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '6.841165',
+	defaultLongitude: '79.965431',
 
 	// Autochange
 	autoChangeTheme: true,
